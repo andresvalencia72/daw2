3a33cf3e0efb53a9492570f595d2515a161a2868
Andrés Valencia <andresvalencia7270@gmail.com>
2023-10-05 21:20:30 +0200

    practica 1 teoría servidores basico

---
+++ servidores/practicas/01_practica_teoria/proyecto1.php
@@ -0,0 +1,112 @@
+<html>
+   <head></head>
+   <body>
+      <form name=formulario method=post action=proyecto1.php>
+         Votante: 
+         <select name=votante>
+            <option value=11>Alumno 11</option>
+            <option value=12>Alumno 12</option>
+            <option value=13>Alumno 13</option>
+            <option value=14>Alumno 14</option>
+            <option value=15>Alumno 15</option>
+            <option value=21>Alumno 21</option>
+            <option value=22>Alumno 22</option>
+            <option value=23>Alumno 23</option>
+            <option value=24>Alumno 24</option>
+            <option value=25>Alumno 25</option>
+            <option value=31>Alumno 31</option>
+            <option value=32>Alumno 32</option>
+            <option value=33>Alumno 33</option>
+            <option value=34>Alumno 34</option>
+            <option value=35>Alumno 35</option>
+            <option value=36>Alumno 36</option>
+         </select>
+         <br>Elige a:
+         <br>
+         <input type=radio name=votado value=blanco checked> Voto en blanco
+         <br>
+         <input type=radio name=votado value=11> Alumno 11
+         <br><input type=radio name=votado value=12> Alumno 12
+         <br><input type=radio name=votado value=13> Alumno 13
+         <br><input type=radio name=votado value=14> Alumno 14
+         <br><input type=radio name=votado value=15> Alumno 15<br><input type=radio name=votado value=21> Alumno 21<br><input type=radio name=votado value=22> Alumno 22<br><input type=radio name=votado value=23> Alumno 23<br><input type=radio name=votado value=24> Alumno 24<br><input type=radio name=votado value=25> Alumno 25<br><input type=radio name=votado value=31> Alumno 31<br><input type=radio name=votado value=32> Alumno 32<br><input type=radio name=votado value=33> Alumno 33<br><input type=radio name=votado value=34> Alumno 34<br><input type=radio name=votado value=35> Alumno 35<br><input type=radio name=votado value=36> Alumno 36<br><input type=hidden name=datos value='a:17:{s:6:"blanco";a:3:{s:6:"nombre";s:14:"Voto en blanco";s:4:"vota";b:1;s:5:"votos";i:0;}i:11;a:3:{s:6:"nombre";s:9:"Alumno 11";s:4:"vota";b:0;s:5:"votos";i:0;}i:12;a:3:{s:6:"nombre";s:9:"Alumno 12";s:4:"vota";b:0;s:5:"votos";i:0;}i:13;a:3:{s:6:"nombre";s:9:"Alumno 13";s:4:"vota";b:0;s:5:"votos";i:0;}i:14;a:3:{s:6:"nombre";s:9:"Alumno 14";s:4:"vota";b:0;s:5:"votos";i:0;}i:15;a:3:{s:6:"nombre";s:9:"Alumno 15";s:4:"vota";b:0;s:5:"votos";i:0;}i:21;a:3:{s:6:"nombre";s:9:"Alumno 21";s:4:"vota";b:0;s:5:"votos";i:0;}i:22;a:3:{s:6:"nombre";s:9:"Alumno 22";s:4:"vota";b:0;s:5:"votos";i:0;}i:23;a:3:{s:6:"nombre";s:9:"Alumno 23";s:4:"vota";b:0;s:5:"votos";i:0;}i:24;a:3:{s:6:"nombre";s:9:"Alumno 24";s:4:"vota";b:0;s:5:"votos";i:0;}i:25;a:3:{s:6:"nombre";s:9:"Alumno 25";s:4:"vota";b:0;s:5:"votos";i:0;}i:31;a:3:{s:6:"nombre";s:9:"Alumno 31";s:4:"vota";b:0;s:5:"votos";i:0;}i:32;a:3:{s:6:"nombre";s:9:"Alumno 32";s:4:"vota";b:0;s:5:"votos";i:0;}i:33;a:3:{s:6:"nombre";s:9:"Alumno 33";s:4:"vota";b:0;s:5:"votos";i:0;}i:34;a:3:{s:6:"nombre";s:9:"Alumno 34";s:4:"vota";b:0;s:5:"votos";i:0;}i:35;a:3:{s:6:"nombre";s:9:"Alumno 35";s:4:"vota";b:0;s:5:"votos";i:0;}i:36;a:3:{s:6:"nombre";s:9:"Alumno 36";s:4:"vota";b:0;s:5:"votos";i:0;}}'><input type=submit name=boton value=Votar>
+      </form>
+      <hr>
+      <h1>La votacion va asi</h1>
+      <table border=0>
+         <tr>
+            <th>Alumno</th>
+            <th></th>
+            <th>Votos</th>
+         </tr>
+         <tr>
+            <td>Voto en blanco </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 11 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 12 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 13 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 14 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 15 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 21 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 22 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 23 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 24 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 25 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 31 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 32 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 33 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 34 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 35 </td>
+            <td>0</td>
+         </tr>
+         <tr>
+            <td>Alumno 36 </td>
+            <td>0</td>
+         </tr>
+      </table>
+   </body>
+</html>
